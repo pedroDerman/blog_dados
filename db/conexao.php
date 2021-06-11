f451683498a7032524f530276868066c64534b35
pedro derman <dermanpedro2@gmail.com>
2021-06-11 13:21:55 -0300

    conexao_ext

--- db/conexao.php
+++ db/conexao.php
@@ -2,10 +2,19 @@
 
 //Pagina de coneção com o banco de dados.
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname="dados_blog";
+//Banco de dados local
+//$servername = "localhost";
+//$username = "root";
+//$password = "";
+//$dbname="dados_blog";
+
+//Banco de dados externo 
+$servername = "	qao3ibsa7hhgecbv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$username = "mclf4x8bwai9xwr4";
+$password = "xub4v47d5cts31ba	
+";
+$dbname="or1h0wik29d2piyu";
+
 
 try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8" ,$username, $password);
