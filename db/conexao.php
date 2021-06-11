1db14c55671962c7c075a4818a3bb07dedfa8efe
pedro derman <dermanpedro2@gmail.com>
2021-06-11 16:48:57 -0300

    main

--- db/conexao.php
+++ db/conexao.php
@@ -9,15 +9,15 @@ $password = "";
 $dbname="dados_blog"; */ 
 
 //Banco de dados externo
-
-$servername = "mysql://mclf4x8bwai9xwr4:xub4v47d5cts31ba@qao3ibsa7hhgecbv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/or1h0wik29d2piyu";
+ 
+$servername = "qao3ibsa7hhgecbv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 $username   = "mclf4x8bwai9xwr4";
 $password   = "xub4v47d5cts31ba	";
 $dbname     = "or1h0wik29d2piyu";
 
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8" ,$username, $password);
+  $conn = new PDO("mysql:host=$servername;port=3306;dbname=$dbname;charset=utf8" ,$username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Conectado Feita Com Sucesso";
