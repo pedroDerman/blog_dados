3d4d89d6109a6a27d91dee06bcfb2499d16ad1f0
pedro derman <dermanpedro2@gmail.com>
2021-06-11 16:52:06 -0300

    mian

--- db/conexao.php
+++ db/conexao.php
@@ -12,12 +12,12 @@ $dbname="dados_blog"; */
  
 $servername = "qao3ibsa7hhgecbv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 $username   = "mclf4x8bwai9xwr4";
-$password   = "xub4v47d5cts31ba	";
+$password   = "xub4v47d5cts31ba";
 $dbname     = "or1h0wik29d2piyu";
 
 
 try {
-  $conn = new PDO("mysql:host=$servername;port=3306;dbname=$dbname;charset=utf8" ,$username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8" ,$username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Conectado Feita Com Sucesso";
