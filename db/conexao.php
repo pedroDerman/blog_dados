7c129a32efe8c1474944e944afec5505d3e7547c
pedro derman <dermanpedro2@gmail.com>
2021-06-11 13:39:35 -0300

    comit

--- db/conexao.php
+++ db/conexao.php
@@ -9,10 +9,9 @@
 //$dbname="dados_blog";
 
 //Banco de dados externo 
-$servername = "	qao3ibsa7hhgecbv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$servername = "qao3ibsa7hhgecbv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 $username = "mclf4x8bwai9xwr4";
-$password = "xub4v47d5cts31ba	
-";
+$password = "xub4v47d5cts31ba	";
 $dbname="or1h0wik29d2piyu";
 
 
